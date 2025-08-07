68fc67bc07891b99778de48ef86705626c2cebd4
Dhruv-Jain3105 <dj.dhruvjain3105@gmail.com>
2025-08-07 20:15:23 +0530

    Create print_even_values.cpp

---
+++ print_even_values.cpp
@@ -0,0 +1,24 @@
+/*
+Name: Dhruv Jain
+PRN:-24070123042
+Batch:-A2
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i=0;i<=10;){
+        cout << i << "\n";
+        i=i+2;
+    }
+    return 0;
+}
+/*
+Output:-
+0
+2
+4
+6
+8
+10
+*/
