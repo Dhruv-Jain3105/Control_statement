2d0001a8148920fda2307a40eb99799cde74a3a6
Dhruv-Jain3105 <dj.dhruvjain3105@gmail.com>
2025-08-07 20:14:26 +0530

    Create print_SIT.cpp

---
+++ print_SIT.cpp
@@ -0,0 +1,22 @@
+/*
+Name: Dhruv Jain
+PRN:-24070123042
+Batch:-A2
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i=0;i<5;i++){
+        cout << "SIT"<< "\n";
+    }
+    return 0;
+}
+/*
+Output:-
+SIT
+SIT
+SIT
+SIT
+SIT
+*/
